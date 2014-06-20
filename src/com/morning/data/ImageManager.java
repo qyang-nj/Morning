baeb4c91b144a256250bae41166f7d8c7ba6e0d4
Qing Yang <yangq.nj@gmail.com>
2014-06-19 20:08:21 -0400

    Add image database helper.

---
+++ src/com/morning/data/ImageManager.java
@@ -0,0 +1,22 @@
+package com.morning.data;
+
+import android.graphics.Bitmap;
+
+public class ImageManager {
+	
+	private ImageManager() {
+		
+	}
+	
+	private String getImageUrl() {
+		return "";
+	}
+	
+	private Bitmap downloadImage() {
+		return null;
+	}
+	
+	public Bitmap getImage() {
+		return null;
+	}
+}
