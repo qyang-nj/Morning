eec4d7903656147332e9ac38a433e4642e9eae7e
Qing Yang <yangq.nj@gmail.com>
2014-07-09 00:05:28 -0400

    Maximize the volume while ringing.

--- src/com/morning/AlertActivity.java
+++ src/com/morning/AlertActivity.java
@@ -1,18 +1,13 @@
 package com.morning;
 
-import java.io.IOException;
 import java.util.Calendar;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -24,7 +19,7 @@ import com.morning.data.ImageManager;
 
 public class AlertActivity extends Activity {
 	private AlarmEntity alarm = null;
-	private MediaPlayer ringtonePlayer = null;
+	private Ringtone ringtone;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +27,7 @@ public class AlertActivity extends Activity {
 
 		setContentView(R.layout.activity_alert);
 		getActionBar().hide();
-		
+
 		Intent in = getIntent();
 		alarm = in.getParcelableExtra(Constants.INTEND_KEY_ALARM);
 		assert alarm != null;
@@ -44,56 +39,33 @@ public class AlertActivity extends Activity {
 		ImageView imgShown = (ImageView) findViewById(R.id.imgShown);
 		imgShown.setImageBitmap(ImageManager.getRandomImage());
 
-		
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
 		// while interacting with the UI.
 		findViewById(R.id.btn_snooze).setOnTouchListener(
 				mDelayHideTouchListener);
-		findViewById(R.id.btn_off).setOnTouchListener(
-				mDelayHideTouchListener);
+		findViewById(R.id.btn_off).setOnTouchListener(mDelayHideTouchListener);
 
 		Uri uri = alarm.getRingtone() == null ? RingtoneManager
 				.getDefaultUri(RingtoneManager.TYPE_ALARM) : Uri.parse(alarm
 				.getRingtone());
 
-		try {
-			MediaPlayer player = new MediaPlayer();
-			player.setDataSource(this, uri);
-			final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-			if (audioManager.getStreamVolume(AudioManager.STREAM_RING) != 0) {
-				player.setAudioStreamType(AudioManager.STREAM_RING);
-				player.setLooping(true);
-				player.prepare();
-				player.start();
-			}
-			ringtonePlayer = player;
-		} catch (IllegalArgumentException e) {
-			Log.e(Constants.TAG, e.getMessage());
-		} catch (SecurityException e) {
-			Log.e(Constants.TAG, e.getMessage());
-		} catch (IllegalStateException e) {
-			Log.e(Constants.TAG, e.getMessage());
-		} catch (IOException e) {
-			Log.e(Constants.TAG, e.getMessage());
-		}
+		ringtone = new Ringtone(this, uri);
+		ringtone.play();
 	}
 
 	@Override
 	protected void onStop() {
 		super.onPause();
-		
+
 		if (alarm.getRepeat() == 0) {
 			alarm.setEnabled(false);
 			alarm.commit();
 		}
-		
-		if (ringtonePlayer.isPlaying()) {
-			ringtonePlayer.stop();
-		}
+		ringtone.stop();
 	}
 
 	/**
@@ -103,7 +75,7 @@ public class AlertActivity extends Activity {
 	 */
 	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
 		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {			
+		public boolean onTouch(View view, MotionEvent motionEvent) {
 			int viewId = view.getId();
 			if (viewId == R.id.btn_snooze) {
 				alarm.setSnooze(Constants.SNOOZE_TIME);
---
+++ src/com/morning/Ringtone.java
@@ -0,0 +1,72 @@
+package com.morning;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.util.Log;
+
+public class Ringtone implements MediaPlayer.OnPreparedListener {
+	private AudioManager audioManager;
+	private MediaPlayer audioPlayer;
+	private int userVolume;
+	private boolean isPrepared = false;
+	private boolean isPlayed = false;
+	private int streamType;
+
+	public Ringtone(Context context, Uri uri) {
+		streamType = AudioManager.STREAM_ALARM;
+		audioManager = (AudioManager) context
+				.getSystemService(Context.AUDIO_SERVICE);
+
+		audioPlayer = new MediaPlayer();
+		try {
+			audioPlayer.setDataSource(context, uri);
+		} catch (IllegalArgumentException e) {
+			Log.e(Constants.TAG, e.getMessage());
+		} catch (SecurityException e) {
+			Log.e(Constants.TAG, e.getMessage());
+		} catch (IllegalStateException e) {
+			Log.e(Constants.TAG, e.getMessage());
+		} catch (IOException e) {
+			Log.e(Constants.TAG, e.getMessage());
+		}
+		audioPlayer.setAudioStreamType(streamType);
+		audioPlayer.setLooping(true);
+		audioPlayer.setOnPreparedListener(this);
+		audioPlayer.prepareAsync();
+	}
+
+	@Override
+	public void onPrepared(MediaPlayer mp) {
+		isPrepared = true;
+		if (isPlayed) {
+			audioPlayer.start();
+		}
+	}
+
+	public void play() {
+		userVolume = audioManager.getStreamVolume(streamType);
+		audioManager.setStreamVolume(streamType,
+				audioManager.getStreamMaxVolume(streamType),
+				AudioManager.FLAG_PLAY_SOUND);
+
+		if (isPrepared) {
+			audioPlayer.start();
+		} else {
+			isPlayed = true;
+		}
+	}
+
+	public void stop() {
+		if (audioPlayer != null && audioPlayer.isPlaying()) {
+			audioPlayer.stop();
+			audioPlayer.reset();
+			audioManager.setStreamVolume(streamType, userVolume,
+					AudioManager.FLAG_PLAY_SOUND);
+			isPrepared = false;
+		}
+	}
+}
