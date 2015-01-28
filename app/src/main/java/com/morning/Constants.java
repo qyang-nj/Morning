8a6749c9033d3fc070c67948ef9c88ab85fb3d83
Qing Yang <yangq.nj@gmail.com>
2015-01-28 01:38:33 -0500

    Remove unused files.

--- app/src/main/java/com/morning/Constants.java
+++ app/src/main/java/com/morning/Constants.java
@@ -6,5 +6,4 @@ public class Constants {
 
     public static final String TAG = "morning";
     public static final String URL_GET_IMAGE = "http://floating-earth-4908.herokuapp.com/clock_images/random_image_url";
-    public static final String INTEND_KEY_ALARM = "com.morning.AlarmEntity";
 }
